87aee7d44a9ef6603d484adf725dbf7c8619f678
Tobias Wicky <tobias.wicky@meteoswiss.ch>
2023-02-08 10:47:24 +0100

    initial commit

---
+++ USAGE.md
@@ -0,0 +1,13 @@
+# Usage
+
+Use demo project in a project:
+
+```python
+import demo_project
+```
+
+Before running, make sure to activate your project environment:
+
+```bash
+conda activate demo-project
+```
---
+++ src/demo_project/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for demo project."""
+# Standard library
+import importlib.metadata
+
+__author__ = "Tobias Wicky"
+__email__ = "tobias.wicky@meteoswiss.ch"
+__version__ = importlib.metadata.version(__package__)
+
+del importlib
